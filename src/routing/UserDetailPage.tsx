4da4e4fefd4faf65ea341df9a59b36eabd595150
Andrew Grebennikov <av.grebennikov@gmail.com>
2023-09-03 19:30:32 -0400

    Add basic routing

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,5 +1,11 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
-  return <p>User</p>;
+  const params = useParams();
+  const [searchParams] = useSearchParams();
+  const location = useLocation();
+  console.log(params, searchParams.get("name"), location);
+  return <p>User {params.id}</p>;
 };
 
 export default UserDetailPage;
