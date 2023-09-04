989894c41139a5a32c949f74a13c1e3296d8f21a
Andrew Grebennikov <av.grebennikov@gmail.com>
2023-09-03 20:06:01 -0400

    Show error

--- src/routing/ErrorPage.tsx
+++ src/routing/ErrorPage.tsx
@@ -1,10 +1,20 @@
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
 const ErrorPage = () => {
-  return (
-    <>
-      <h1>Oops...</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-    </>
-  );
+  const error = useRouteError()
+  console.warn("My error", error);
+  if (isRouteErrorResponse(error)) {
+    return (
+      <>
+        <h1>Oops...</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+      </>
+    );
+  }
+
+  return (<>
+  <p>Something went wrong</p>
+  </>);
 };
 
 export default ErrorPage;
